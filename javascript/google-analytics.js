0701a448ed253247a5205a854a63cb565f3af75a
zkyh0ok <34177365+zkyh0ok@users.noreply.github.com>
2025-01-14 05:30:41 -0300

    Update google-analytics.js

--- javascript/google-analytics.js
+++ javascript/google-analytics.js
@@ -1,23 +1,8 @@
-var _gaq = _gaq || [];
-_gaq.push([ '_setAccount', 'UA-26076327-1' ]);
-_gaq.push([ '_trackPageview' ]);
+// google-analytics.js - código para Google Analytics (puedes desactivarlo si no lo necesitas)
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-/* Non-Personal Statistics */
-_gaq.push([ '_trackEvent', 'Version', chrome.app.getDetails().version ]);
-if(localStorage.getItem("showbmb") !== null) {
-  _gaq.push([ '_trackEvent', 'Bookmark Bar', localStorage.getItem("showbmb") ]);
-}
-if(localStorage.getItem("perm-grid") !== null) {
-  _gaq.push([ '_trackEvent', 'Permanent Grid', localStorage.getItem("perm-grid") ]);
-}
-if(localStorage.getItem("bg-img-css") !== null && localStorage.getItem("bg-img-css") !== "") {
-  _gaq.push([ '_trackEvent', 'Custom Background', "Yes" ]);
-} else {
-  _gaq.push([ '_trackEvent', 'Custom Background', "No" ]);
-}
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-XXXXXXX-X', 'auto');
+ga('send', 'pageview');
